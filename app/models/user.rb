b7007620843710b987827aa6897cc148599c1f2b
kenyoshizawa <harborseal2521@gmail.com>
2021-02-24 19:09:31 +0900

    [Add] Userオブジェクトに対するバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :user_events, dependent: :destroy
+  has_many :events, through: :user_events
+
+  enum role: { friend: 0, organizer: 1 }
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :role, presence: true
 end

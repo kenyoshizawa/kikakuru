3513ee2b68441d9587f004a8a30e38ba151addb5
kenyoshizawa <harborseal2521@gmail.com>
2022-01-27 17:24:14 +0900

    [Add] Fullcalendar作成に必要なAssetsをapplication.jsに追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require moment
+//= require fullcalendar
+//= require fullcalendar/locale-all
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets

214bbbfa992b55fe28b400e8889074d02e6e214a
kenyoshizawa <harborseal2521@gmail.com>
2022-05-10 16:36:17 +0900

    [Fix] カレンダーの日付クリック時に、背景色と日にちの色を変更する機能を追加

--- app/assets/javascripts/fullcalendar/main.js
+++ app/assets/javascripts/fullcalendar/main.js
@@ -9,9 +9,18 @@ $(function() {
       // もし配列の中に同じ値が存在しない場合select_date.indexOf(datetime)は-1を返す
       if ( select_date.indexOf(datetime) === -1 ) {
         select_date.push(datetime); // 配列にクリックした日付を追加する（日付の重複なし）
+        // 背景色変更
+        $(this).css({'background-color': 'yellow'});
+        // 日付の色変更
+        $('[data-date="' + datetime + '"]').css('color','red');
+
       } else {
         var double_date = select_date.indexOf(datetime)
         select_date.splice(double_date, 1); // 重複する値のkeyの位置から1番目を削除（日付の重複あり）
+        // 背景色変更
+        $(this).css({'background-color': ''});
+        // 日付の色変更
+        $('[data-date="' + datetime + '"]').css('color','');
       }
 
       console.log(select_date);

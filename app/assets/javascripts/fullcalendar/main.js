68f432f8dd7e9c8c72ceb4909e0fb7bc20e997ab
kenyoshizawa <harborseal2521@gmail.com>
2022-05-13 16:32:28 +0900

    [Fix] カレンダーの高さをautoにしてスクロールバーを表示させない

--- app/assets/javascripts/fullcalendar/main.js
+++ app/assets/javascripts/fullcalendar/main.js
@@ -8,6 +8,7 @@ $(function() {
       center: 'title',
       right: 'next'
     },
+    contentHeight: 'auto',
     dayClick: function(date) {
       // クリックした日付を"YYYY-MM-DD"形式で変数に代入
       var datetime = moment(date).format("YYYY-MM-DD");
@@ -16,9 +17,9 @@ $(function() {
       if ( select_date.indexOf(datetime) === -1 ) {
         select_date.push(datetime); // 配列にクリックした日付を追加する（日付の重複なし）
         // 背景色変更
-        $(this).css({'background-color': 'yellow'});
+        $(this).css({'background-color': '#e3165c'});
         // 日付の色変更
-        $('[data-date="' + datetime + '"]').css('color','red');
+        $('[data-date="' + datetime + '"]').css('color','#fff');
 
       } else {
         var double_date = select_date.indexOf(datetime)

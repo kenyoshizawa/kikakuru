be5ed5b77b12583183c9410f37b161b4beefe24e
kenyoshizawa <harborseal2521@gmail.com>
2022-05-20 16:24:03 +0900

    [Fix] 企画開始時間と終了時間でClockPickerの設定を分ける

--- app/assets/javascripts/clockpicker/main.js
+++ app/assets/javascripts/clockpicker/main.js
@@ -1,4 +1,11 @@
 $(function() {
-  $('.clockpicker').clockpicker({
+  $('.started-clockpicker').clockpicker({
+    // default: `13:00`,
+    autoclose: true,
+  });
+
+  $('.finished-clockpicker').clockpicker({
+    // default: `19:00`,
+    autoclose: true,
   });
 });

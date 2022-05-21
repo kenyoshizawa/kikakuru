828fd91c4c9b0216a55a6934e7f87fad4d8b2f23
kenyoshizawa <harborseal2521@gmail.com>
2022-05-21 19:19:07 +0900

    [Fix] デフォルトの言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Kikakuru
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone= :local
     config.active_record.time_zone_aware_types = [:datetime, :time]
+    config.i18n.default_locale = :ja
   end
 end

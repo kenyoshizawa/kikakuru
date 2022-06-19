a7583f9a12501a89a3cbca8ec9fc243510eae1e1
kenyoshizawa <harborseal2521@gmail.com>
2022-06-19 13:30:59 +0900

    [Add] validatorsディレクトリ追加

---
+++ app/validators/place_format_validator.rb
@@ -0,0 +1,11 @@
+class PlaceFormatValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    googlemap_url = %w(
+      https://www.google.co.jp//maps/place/
+      https://goo.gl/maps/
+    )
+    unless value.in?(googlemap_url)
+      record.errors[attribute] << (options[:message] || 'を貼り付けてください')
+    end
+  end
+end

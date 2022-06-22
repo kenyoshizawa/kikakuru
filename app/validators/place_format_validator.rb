f3c5af3169ad19b84a777653a16cbf7ddb0693ba
kenyoshizawa <harborseal2521@gmail.com>
2022-06-22 19:53:12 +0900

    [Fix] Eventモデルplace属性のバリデーションに使うメソッドをstart_with?に変更

--- app/validators/place_format_validator.rb
+++ app/validators/place_format_validator.rb
@@ -1,10 +1,11 @@
 class PlaceFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     googlemap_url = %w(
-      https://www.google.co.jp//maps/place/
+      https://www.google.co.jp/maps/place/
       https://goo.gl/maps/
     )
-    unless value.in?(googlemap_url)
+
+    unless value.start_with?(*googlemap_url)
       record.errors[attribute] << (options[:message] || 'を貼り付けてください')
     end
   end

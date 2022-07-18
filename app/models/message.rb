f54517a57a90ea9ac4dfc4c08366b8083f7c5429
kenyoshizawa <harborseal2521@gmail.com>
2022-07-18 15:49:58 +0900

    [Add] Messageモデルcontent属性にバリデーション追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :event
   after_create_commit { MessageBroadcastJob.perform_later self }
+
+  validates :content, presence: true
 end

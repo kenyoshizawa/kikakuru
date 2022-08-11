d14ce74c1dba5f16ef8d2f08916db38ed8961824
kenyoshizawa <harborseal2521@gmail.com>
2022-08-11 16:58:28 +0900

    [Fix] sockets/puma.sockのtmp_pathを絶対パスに変更

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -2,7 +2,7 @@
 environment "production"
 
 # UNIX Socketへのバインド
-tmp_path = "#{File.expand_path("../../..", __FILE__)}/tmp"
+tmp_path = "/var/www/kikakuru/tmp"
 bind "unix://#{tmp_path}/sockets/puma.sock"
 
 # スレッド数とWorker数の指定

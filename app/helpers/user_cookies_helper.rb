d86d082b1793b5cf48e3488a91e9604d548d9c8c
kenyoshizawa <harborseal2521@gmail.com>
2023-10-30 12:20:58 +0900

    [Remove] app/helpers/user_cookies_helper.rbのlogged_in?メソッド、forgetメソッド削除

--- app/helpers/user_cookies_helper.rb
+++ app/helpers/user_cookies_helper.rb
@@ -6,13 +6,4 @@ module UserCookiesHelper
   def current_user
     @current_user ||= User.find_by(id: cookies.signed[:user_id])
   end
-
-  # def logged_in?
-  #   !current_user.nil?
-  # end
-
-  def forget(current_user)
-    @current_user = nil
-    cookies.delete(:user_id)
-  end
 end
